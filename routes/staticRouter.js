8653a249d6f8c2d947900c6a2169e0e9591201fd
Abhishek Bharti <bhartiabhishek310@gmail.com>
2025-01-19 01:06:19 +0530

    created table on page to show all details about shortened urls

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-router.get("/",(req,res)=>{
-    res.render("home")
+const URL = require('../models/urls');
+
+router.get("/",async(req,res)=>{
+    const allurls = await URL.find({});
+    res.render("home",{
+        urls: allurls,
+    });
 })
 module.exports = router;
